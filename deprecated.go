6033cc70ff37c22f4e8a4277b989077ffca464dc
Raúl Kripalani <raul@protocol.ai>
2019-04-11 11:29:04 +0100

    WIP consolidate go-libp2p skeleton.

---
+++ deprecated.go
@@ -0,0 +1,12 @@
+package ifconnmgr
+
+import moved "github.com/libp2p/go-libp2p/skel/connmgr"
+
+// Deprecated: use github.com/libp2p/go-libp2p/skel/connmgr.ConnManager instead.
+type ConnManager = moved.ConnManager
+
+// Deprecated: use github.com/libp2p/go-libp2p/skel/connmgr.TagInfo instead.
+type TagInfo = moved.TagInfo
+
+// Deprecated: use github.com/libp2p/go-libp2p/skel/connmgr.NullConnMgr instead.
+type NullConnMgr = moved.NullConnMgr
--- go.mod
+++ go.mod
@@ -1,7 +1,8 @@
 module github.com/libp2p/go-libp2p-interface-connmgr
 
 require (
-	github.com/libp2p/go-libp2p-net v0.0.1
+	github.com/libp2p/go-libp2p v0.0.11
+	github.com/libp2p/go-libp2p-net v0.0.2
 	github.com/libp2p/go-libp2p-peer v0.0.1
 	github.com/multiformats/go-multiaddr v0.0.1
 )
