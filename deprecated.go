f20b42bc16663f7aa89a968be6a1d5d440a60511
Raúl Kripalani <raul@protocol.ai>
2019-04-16 18:05:56 +0100

    switch to go-libp2p-core.

--- deprecated.go
+++ deprecated.go
@@ -1,12 +1,12 @@
 package ifconnmgr
 
-import moved "github.com/libp2p/go-libp2p/skel/connmgr"
+import moved "github.com/libp2p/go-libp2p-core/connmgr"
 
-// Deprecated: use github.com/libp2p/go-libp2p/skel/connmgr.ConnManager instead.
+// Deprecated: use github.com/libp2p/go-libp2p-core/connmgr.ConnManager instead.
 type ConnManager = moved.ConnManager
 
-// Deprecated: use github.com/libp2p/go-libp2p/skel/connmgr.TagInfo instead.
+// Deprecated: use github.com/libp2p/go-libp2p-core/connmgr.TagInfo instead.
 type TagInfo = moved.TagInfo
 
-// Deprecated: use github.com/libp2p/go-libp2p/skel/connmgr.NullConnMgr instead.
+// Deprecated: use github.com/libp2p/go-libp2p-core/connmgr.NullConnMgr instead.
 type NullConnMgr = moved.NullConnMgr
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/libp2p/go-libp2p-interface-connmgr
 
-require (
-	github.com/libp2p/go-libp2p v0.0.11
-	github.com/libp2p/go-libp2p-net v0.0.2
-	github.com/libp2p/go-libp2p-peer v0.0.1
-	github.com/multiformats/go-multiaddr v0.0.1
-)
+require github.com/libp2p/go-libp2p-core v0.0.0-20190416150442-54a1b70f07da
